76c509f9b45449c6abb9b1f674713bd822823934
lordYema <ameywani2012@gmail.com>
2024-06-18 16:04:29 +0530

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className=" text-3xl text-emerald-950 font-bold">
+      Welcome to WIEE SPIT
+    </div>
+  );
+}
+
+export default App;
